356cc263a114ff06358e50fdf4d29a92453924f2
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2024-07-17 09:38:48 +0530

    Delete typescript-course/8.functions directory

--- typescript-course/8.functions/example.js
+++
@@ -1,9 +0,0 @@
-function addNumber(a,b)
-{
-    return a + b;
-
-}
-
-var result = addNumber(1,2);
-result = addNumber("1","2");
-console.log(result);
--- typescript-course/8.functions/example2.ts
+++
@@ -1,9 +0,0 @@
-function addNumbers(a:number,b:number)
-{
-    return a + b;
-
-}
-
-var result = addNumbers(1,2);
-result = addNumbers(3,2);
-console.log(result);
